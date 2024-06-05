a3a5b0a4a81c7622dec892c1a4ce48ebb32283cb
SebasTorresDev <sebas.developer@outlook.com>
2024-06-05 02:51:00 -0500

    Committing changes to .gitignore

---
+++ .angular/cache/17.3.8/vite/deps/primeng_tooltip.js
@@ -0,0 +1,14 @@
+import {
+  Tooltip,
+  TooltipModule
+} from "./chunk-3N74NSAJ.js";
+import "./chunk-TKJMWDM5.js";
+import "./chunk-MYGHXXHX.js";
+import "./chunk-G7IZU25V.js";
+import "./chunk-HYEAOKGG.js";
+import "./chunk-5JEZL4LT.js";
+export {
+  Tooltip,
+  TooltipModule
+};
+//# sourceMappingURL=primeng_tooltip.js.map
